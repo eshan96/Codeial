cdedf91227bdd689cee01a0e5d4afc96b1860e34
Your Name <eshanjindal9@gmail.com>
2019-10-13 13:27:04 +0530

    add users controller and router

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const homeController = require('../controllers/home_controller')
 console.log('Router called')
 
 router.get('/', homeController.home)
+router.use('/users', require('./users'))
 
 module.exports = router
