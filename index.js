7a81a2a8d8476d6d9ae07d9fdeaa9d1da22940be
Your Name <eshanjindal9@gmail.com>
2019-10-13 10:43:45 +0530

    add a controller and a router for home

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 8000;
 
 
+app.use('/', require('./routes/index'))
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in Running the Server: ${err}`)
